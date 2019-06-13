d77ecb99bf1e50da735cde35761aa6e657082a74
Hung-Tan-Tran <HTT15@my.fsu.edu>
2019-06-13 15:49:12 -0400

    Offset string creation

--- vigenere.cpp
+++ vigenere.cpp
@@ -4,6 +4,8 @@ string encrypt(ifstream & fin, string & key)
 {
     int fileLength;
     int keyLength = key.size();
+    int currFilePos = 0;
+    int currKeyPos = 0;
     string cipherText;
 
     //Get length of file
@@ -11,7 +13,15 @@ string encrypt(ifstream & fin, string & key)
     fileLength = fin.tellg();
     fin.seekg(0, fin.beg);
 
-    printf("%d", fileLength);
+    //Creating offset string
+    while(currFilePos != fileLength)
+    {
+        printf("%c",key[currKeyPos]);
+        currKeyPos++;
+        if(currKeyPos == keyLength)
+            currKeyPos = 0;
+        currFilePos++;
+    }
 
     return cipherText;
 }
